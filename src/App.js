4327da0a52af115d5f563d0c48ba7063239d0a83
NeidaCalsina <sneidacalsina@gmail.com>
2021-07-22 22:43:34 -0500

    Version 4 modificacion de app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import C01componentes from "./componentes/C01componentes";
+import C02contador from "./componentes/C02contador";
+import C03compCss from "./componentes/C03compCss";
+import C04estado from "./componentes/C04estado";
+import C05dobleestado from "./componentes/C05dobleestado";
+import C06variable from "./componentes/C06variable";
+import OperadorTernario from "./componentes/OperadorTernario";
+import Matriz from "./componentes/Matriz";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <C01componentes></C01componentes>
+      <hr/>
+      <C02contador></C02contador>
+      <hr/>
+      <C03compCss></C03compCss>
+      <hr/>
+      <C04estado></C04estado>
+      <hr/>
+      <C05dobleestado></C05dobleestado>
+      <hr/>
+      <C06variable xVariable="Hola mundo..."/>
+      <hr/>
+      <OperadorTernario xEdad = '20' />
+      <hr/>
+      <Matriz />
     </div>
   );
 }
 
 export default App;
+
