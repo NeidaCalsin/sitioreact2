edfed89e153eb4a160deee01492259bb535bca5d
NeidaCalsina <sneidacalsina@gmail.com>
2021-07-22 22:28:35 -0500

    Version 3 modificado en nombre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sitio web en React
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
